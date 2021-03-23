430b9e39859ae91f2ff8cce644c76ce54ac1e517
Louis Rocher <louis.rocher@gmail.com>
2021-03-23 19:08:58 +0100

    add vercel.json

--- .history/components/Footer_20210308153327.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer>
-    <div style="height: 500px; width: 500px;" ref="map"></div>
-  </footer>
-</template>
--- .history/middleware/home_20210303165608.js
+++
@@ -1,7 +0,0 @@
-import http from 'http'
-
-export default function ({ route }) {
-  return http.post('http://arnaudgay.re/wp-json/wp/v2/pages/25?_embed', {
-    url: route.fullPath
-  })
-}
